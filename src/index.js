f56b9692a13fe410abd23f1b81c1706ef471040d
miklevasilevich <miklevasilevich@gmail.com>
2021-08-20 23:35:19 +0300

    feat: add function reverse numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n)).toString();
+  let arr = [];
+  for (let i = 0; i < str.length; i += 1) {
+      arr.push(str[i]);
+  }
+  let res = 0;
+  let mod = 1;
+  for (i = 0; i < arr.length; i ++) {
+    res += arr[i] * mod;
+    mod *= 10;
+  }
+  return res;
 }
